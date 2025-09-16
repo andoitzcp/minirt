49b60e1e52e986d506d696eda5099039d5a961e4
iubieta <ikerp.ubieta@gmail.com>
2025-09-16 20:16:19 +0200

    set and get pixel tests added

--- test/src/canvas.c
+++ test/src/canvas.c
@@ -8,6 +8,12 @@ void expect_color_eq(t_color a, t_color b, char *flag, int i) {
 	cr_expect(float_eq(a.b, b.b), "%s %i: expected b:%f, returned b:%f.", flag, i, b.b, a.b);
 }
 
+void expect_color_is_nan(t_color a,char *flag, int i) {
+	cr_expect(isnan(a.r), "%s %i: expected r:%f, returned r:%f.", flag, i, NAN, a.r);
+	cr_expect(isnan(a.g), "%s %i: expected g:%f, returned g:%f.", flag, i, NAN, a.g);
+	cr_expect(isnan(a.b), "%s %i: expected b:%f, returned b:%f.", flag, i, NAN, a.b);
+}
+
 Test(canvas, init) {
 	char	*flag = "canvas_init";
 	int		i = 1;
@@ -51,13 +57,30 @@ Test(canvas, canvas_set_pixel) {
 
 	t_canvas	*can;
 	t_color		color = color_set(0, 0, 0);
+	int			ret;
 
+	can = canvas_init(10, 10);
+	color = color_set(0, 0, 0);
+	ret = canvas_set_pixel(can, 20, 5, color);
+	cr_expect(ret == 1, "%s %i: expected: %i, returned: %i\n", flag, i++, 1, ret);
+
+	ret = canvas_set_pixel(can, 10, 5, color);
+	cr_expect(ret == 1, "%s %i: expected: %i, returned: %i\n", flag, i++, 1, ret);
+	
+	ret = canvas_set_pixel(can, 5, 20, color);
+	cr_expect(ret == 1, "%s %i: expected: %i, returned: %i\n", flag, i++, 1, ret);
+	
+	ret = canvas_set_pixel(can, 5, 10, color);
+	cr_expect(ret == 1, "%s %i: expected: %i, returned: %i\n", flag, i++, 1, ret);
+	
 	can = canvas_init(10, 10);
 	expect_color_eq(can->image[5][5], color, flag, i++);
 	color = color_set(255, 0, 0);
-	canvas_set_pixel(can, 5, 5, color);
+	ret = canvas_set_pixel(can, 5, 5, color);
+	cr_expect(ret == 0, "%s %i: expected: %i, returned: %i\n", flag, i++, 0, ret);
 	expect_color_eq(can->image[5][5], color, flag, i++);
-	// expect_color_eq(can->image[1][5], color, flag, i++);
+	color = color_set(0, 0, 0);
+	expect_color_eq(can->image[1][5], color, flag, i++);
 }
 
 Test(canvas, canvas_get_pixel) {
@@ -69,14 +92,24 @@ Test(canvas, canvas_get_pixel) {
 	t_color		pixel;
 
 	can = canvas_init(10, 10);
+	pixel = canvas_get_pixel(*can, 20, 5);
+	expect_color_is_nan(pixel, flag, i++);
+
+	pixel = canvas_get_pixel(*can, 10, 5);
+	expect_color_is_nan(pixel, flag, i++);
+	
+	pixel = canvas_get_pixel(*can, 5, 20);
+	expect_color_is_nan(pixel, flag, i++);
+	
+	pixel = canvas_get_pixel(*can, 5, 10);
+	expect_color_is_nan(pixel, flag, i++);
+	
 	pixel = canvas_get_pixel(*can, 5, 5);
 	expect_color_eq(pixel, color, flag, i++);
 	color = color_set(255, 0, 0);
 	canvas_set_pixel(can, 5, 5, color);
 	pixel = canvas_get_pixel(*can, 5, 5);
 	expect_color_eq(pixel, color, flag, i++);
-	// pixel = canvas_get_pixel(*can, 1, 5);
-	// expect_color_eq(pixel, color, flag, i++);
 }
 
 Test(canvas, to_ppm) {
