56750619390f9bfce6cdd74d2bd3ee00d547f48a
iubieta <ikerp.ubieta@gmail.com>
2025-11-20 20:23:07 +0100

    style (norminette): cylinder.c

--- src/cylinder.c
+++ src/cylinder.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*																			  */
+/*														  :::	   ::::::::   */
+/*	 cylinder.c											:+:		 :+:	:+:   */
+/*													  +:+ +:+		  +:+	  */
+/*	 By: iubieta- <iubieta@student.42.fr>			+#+  +:+	   +#+		  */
+/*												  +#+#+#+#+#+	+#+			  */
+/*	 Created: 2025/11/20 20:10:30 by iubieta-		   #+#	  #+#			  */
+/*	 Updated: 2025/11/20 20:10:30 by iubieta-		  ###	########.fr		  */
+/*																			  */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
 t_cylinder	def_cylinder(void)
@@ -13,30 +25,30 @@ t_cylinder	def_cylinder(void)
 }
 
 // TODO Implement new_cylinder NOT FINISHED
-t_cylinder new_cylinder(void)
+t_cylinder	new_cylinder(void)
 {
-    t_cylinder cy;
+	t_cylinder	cy;
 
-    cy.dia = 1;
-    cy.hei = 1;
-    return (cy);
+	cy.dia = 1;
+	cy.hei = 1;
+	return (cy);
 }
 
 // TODO Implement funtion to take raw data cylinder and convert it into matrix
 // and material and set cylinder data
-t_object *new_cylinder_from_raw_data(t_raw_cylinder *rcy, t_amblight *ambl)
+t_object	*new_cylinder_from_raw_data(t_raw_cylinder *rcy, t_amblight *ambl)
 {
-    t_matrix t;
-	t_matrix s;
-    t_matrix r;
-    t_matrix trans;
-    t_material mat;
+	t_matrix	t;
+	t_matrix	s;
+	t_matrix	r;
+	t_matrix	trans;
+	t_material	mat;
 
-    t = matrix_translation(rcy->p.x, rcy->p.y, rcy->p.z);
+	t = matrix_translation(rcy->p.x, rcy->p.y, rcy->p.z);
 	r = matrix_rotation(rcy->v);
-    s = matrix_scalation(rcy->dia, rcy->hei / 2, rcy->dia);
+	s = matrix_scalation(rcy->dia, rcy->hei / 2, rcy->dia);
 	trans = matrix_matrix_mult(t, r);
 	trans = matrix_matrix_mult(trans, s);
-    mat = new_material_from_raw_data(&rcy->col, ambl);
-    return (new_object(ELID_CY, &trans, &mat));
+	mat = new_material_from_raw_data(&rcy->col, ambl);
+	return (new_object(ELID_CY, &trans, &mat));
 }
