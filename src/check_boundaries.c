57604baf345436db448f88f7707d41f59cc4a7c3
iubieta <ikerp.ubieta@gmail.com>
2025-11-20 20:22:33 +0100

    style (norminette): check_boundaries.c

--- src/check_boundaries.c
+++ src/check_boundaries.c
@@ -1,25 +1,37 @@
+/* ************************************************************************** */
+/*																			  */
+/*														  :::	   ::::::::   */
+/*	 check_boundaries.c									:+:		 :+:	:+:   */
+/*													  +:+ +:+		  +:+	  */
+/*	 By: iubieta- <iubieta@student.42.fr>			+#+  +:+	   +#+		  */
+/*												  +#+#+#+#+#+	+#+			  */
+/*	 Created: 2025/11/20 20:09:08 by iubieta-		   #+#	  #+#			  */
+/*	 Updated: 2025/11/20 20:09:08 by iubieta-		  ###	########.fr		  */
+/*																			  */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
-uint8_t check_color_bounds(t_color *color)
+uint8_t	check_color_bounds(t_color *color)
 {
-    if (color->r < 0 || color->r > 255)
-        return (0);
-    if (color->g < 0 || color->g > 255)
-        return (0);
-    if (color->b < 0 || color->b > 255)
-        return (0);
-    return (1);
+	if (color->r < 0 || color->r > 255)
+		return (0);
+	if (color->g < 0 || color->g > 255)
+		return (0);
+	if (color->b < 0 || color->b > 255)
+		return (0);
+	return (1);
 }
 
-uint8_t check_nvector_bounds(t_tuple *vector)
+uint8_t	check_nvector_bounds(t_tuple *vector)
 {
-    if (vector->w != 1)
-        return (0);
-    if (vector->x < 0 || vector->x > 1)
-        return (0);
-    if (vector->y < 0 || vector->y > 1)
-        return (0);
-    if (vector->z < 0 || vector->z > 1)
-        return (0);
-    return (1);
+	if (vector->w != 1)
+		return (0);
+	if (vector->x < 0 || vector->x > 1)
+		return (0);
+	if (vector->y < 0 || vector->y > 1)
+		return (0);
+	if (vector->z < 0 || vector->z > 1)
+		return (0);
+	return (1);
 }
