0b162f2b8e9d6784d4cbb41264897f1b8a4bd020
iubieta <ikerp.ubieta@gmail.com>
2025-11-21 21:44:30 +0100

    style (norminette): lines.c

--- src/lines.c
+++ src/lines.c
@@ -1,70 +1,82 @@
+/* ************************************************************************** */
+/*																			  */
+/*														  :::	   ::::::::   */
+/*	 lines.c											:+:		 :+:	:+:   */
+/*													  +:+ +:+		  +:+	  */
+/*	 By: iubieta- <iubieta@student.42.fr>			+#+  +:+	   +#+		  */
+/*												  +#+#+#+#+#+	+#+			  */
+/*	 Created: 2025/11/21 20:48:41 by iubieta-		   #+#	  #+#			  */
+/*	 Updated: 2025/11/21 20:48:41 by iubieta-		  ###	########.fr		  */
+/*																			  */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
-char ***split_line(char *s)
+char	***split_line(char *s)
 {
-    size_t len;
-    size_t i;
-    void ***line;
+	size_t	len;
+	size_t	i;
+	void	***line;
 
-    len = ft_strlen(s);
-    if (s[len - 1] == '\n')
-        s[len - 1] = '\0';
-    line = (void ***)ft_split(s, ' ');
-    i = 0;
-    while ((char *)line[i] != NULL)
-    {
-        line[i] = (void **)ft_split((char *)line[i], ',');
-        i++;
-    }
-    return ((char ***)line);
+	len = ft_strlen(s);
+	if (s[len - 1] == '\n')
+		s[len - 1] = '\0';
+	line = (void ***)ft_split(s, ' ');
+	i = 0;
+	while ((char *)line[i] != NULL)
+	{
+		line[i] = (void **)ft_split((char *)line[i], ',');
+		i++;
+	}
+	return ((char ***)line);
 }
 
-t_line *build_line_node(t_data *data, char ***line, int type)
+t_line	*build_line_node(t_data *data, char ***line, int type)
 {
-    t_line *node;
+	t_line	*node;
 
-    node = ft_calloc(1, sizeof(t_line));
-    if (node == NULL)
-        ft_exit(data, ERRORS009);
-    node->type = type;
-    node->content = line;
-    node->next = NULL;
-    return (node);
+	node = ft_calloc(1, sizeof(t_line));
+	if (node == NULL)
+		ft_exit(data, ERRORS009);
+	node->type = type;
+	node->content = line;
+	node->next = NULL;
+	return (node);
 }
 
-void append_line_node(t_line **head, t_line *node)
+void	append_line_node(t_line **head, t_line *node)
 {
-    t_line *current;
+	t_line	*current;
 
-    current = *head;
-    if (current == NULL)
-    {
-        *head = node;
-        return ;
-    }
-    while (current->next != NULL)
-        current = current->next;
-    current->next = node;
-    return ;
+	current = *head;
+	if (current == NULL)
+	{
+		*head = node;
+		return ;
+	}
+	while (current->next != NULL)
+		current = current->next;
+	current->next = node;
+	return ;
 }
 
-void process_line(t_data *data, char *s)
+void	process_line(t_data *data, char *s)
 {
-    int i;
-    char ***line;
-    t_line *node;
+	int		i;
+	char	***line;
+	t_line	*node;
 
-    i = 0;
-    while (data->raw.re_el_types[i] != NULL)
-    {
-        if (match(data->raw.re_el_types[i++], s) == 0)
-            continue ;
-        line = split_line(s);
-        node = build_line_node(data, line, i - 1);
-        if (i - 1 != ELID_BLANK_LINE)
-            append_line_node(&(data->raw.lines), node);
-        return ;
-    }
-    ft_exit(data, ERRORS007);
-    return ;
+	i = 0;
+	while (data->raw.re_el_types[i] != NULL)
+	{
+		if (match(data->raw.re_el_types[i++], s) == 0)
+			continue ;
+		line = split_line(s);
+		node = build_line_node(data, line, i - 1);
+		if (i - 1 != ELID_BLANK_LINE)
+			append_line_node(&(data->raw.lines), node);
+		return ;
+	}
+	ft_exit(data, ERRORS007);
+	return ;
 }
