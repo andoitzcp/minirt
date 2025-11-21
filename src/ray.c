a8a5e08a2cab2a99184cb553588b20137170bd52
iubieta <ikerp.ubieta@gmail.com>
2025-11-21 22:19:18 +0100

    style (norminette): ray.c

--- src/ray.c
+++ src/ray.c
@@ -1,26 +1,38 @@
+/* ************************************************************************** */
+/*																			  */
+/*														  :::	   ::::::::   */
+/*	 ray.c												:+:		 :+:	:+:   */
+/*													  +:+ +:+		  +:+	  */
+/*	 By: iubieta- <iubieta@student.42.fr>			+#+  +:+	   +#+		  */
+/*												  +#+#+#+#+#+	+#+			  */
+/*	 Created: 2025/11/21 21:56:43 by iubieta-		   #+#	  #+#			  */
+/*	 Updated: 2025/11/21 21:56:43 by iubieta-		  ###	########.fr		  */
+/*																			  */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
-t_ray new_ray(t_tuple origin, t_tuple direction)
+t_ray	new_ray(t_tuple origin, t_tuple direction)
 {
-    t_ray r;
+	t_ray	r;
 
-    r.o = origin;
-    r.d = direction;
-    r.i = NULL;
-    return (r);
+	r.o = origin;
+	r.d = direction;
+	r.i = NULL;
+	return (r);
 }
 
 // TODO revisar juntos
-t_tuple position(t_ray ray, float t)
+t_tuple	position(t_ray ray, float t)
 {
-    t_tuple pret;
+	t_tuple	pret;
 
-    pret = tuple_scale_up(ray.d, t);
-    pret = tuple_add(pret, ray.o);
-    return (pret);
+	pret = tuple_scale_up(ray.d, t);
+	pret = tuple_add(pret, ray.o);
+	return (pret);
 }
 
-t_tuple reflect(t_tuple in, t_tuple normal)
+t_tuple	reflect(t_tuple in, t_tuple normal)
 {
 	float	dot;
 	t_tuple	aux;
