a32d7b54389dd1548b8d9dad6346540a86084867
iubieta <ikerp.ubieta@gmail.com>
2025-11-23 19:37:18 +0100

    wip (lighting): hardcoded abr, parse error

--- src/lighting.c
+++ src/lighting.c
@@ -18,6 +18,8 @@ t_color	ambient_lighting(t_material m, t_light l)
 	t_color	ambient;
 
 	ambient = color_blend(m.color, l.col);
+	ambient = color_scale_up(ambient, 0.3);
+	// ambient = color_scale_up(ambient, l.abr);
 	ambient = color_scale_up(ambient, m.ambient);
 	return (ambient);
 }
@@ -27,6 +29,8 @@ t_color	diffuse_lighting(t_material m, t_light l, float ln_dot)
 	t_color	diffuse;
 
 	diffuse = color_blend(m.color, l.col);
+	diffuse = color_scale_up(diffuse, 0.3);
+	// diffuse = color_scale_up(diffuse, l.abr);
 	diffuse = color_scale_up(diffuse, m.diffuse);
 	diffuse = color_scale_up(diffuse, ln_dot);
 	return (diffuse);
@@ -73,6 +77,7 @@ int	is_shadowed(t_world *w, t_tuple p)
 
 /* } */
 
+// TODO añadir amblight en comps y utilizarla para el ambient lighting
 t_color	lighting(t_comps comps)
 {
 	t_color	color;
--- src/minirt.c
+++ src/minirt.c
@@ -45,8 +45,8 @@ int	main(int argc, char **argv)
 	t_canvas	*canvas;
 	t_gui		gui;
 
-	data.raw.res.x_sz = 900;
-	data.raw.res.y_sz = 500;
+	data.raw.res.x_sz = 400;
+	data.raw.res.y_sz = 200;
 	if (argc != 2)
 		exit(1);
 	if (is_valid_input_file(argv[1]) == 0)
