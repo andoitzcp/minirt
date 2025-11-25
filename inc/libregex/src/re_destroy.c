31df0fe31f96fb00ba30c3d702a5fb83b7199297
andoitzcp <andoitzcp@gmail.com>
2025-11-25 18:20:38 +0100

    feat[regex]: add destroy regex function

---
+++ inc/libregex/src/re_destroy.c
@@ -0,0 +1,17 @@
+#include "libregex.h"
+
+void re_destroy(t_re *re)
+{
+    t_re *node;
+    t_re *p;
+
+    node = re;
+    while (node)
+    {
+        p = node;
+        node = node->next;
+        free(p);
+        p = NULL;
+    }
+    return ;
+}
